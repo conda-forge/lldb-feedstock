186412b1cde27f88fcd2d0cdc526e6e424ec536f
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-27 15:14:20 +0200

    Use FindPython3 on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,11 +18,12 @@ cmake -G "Ninja" ^
     -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON ^
     -DLLDB_ENABLE_PYTHON=ON ^
     -DLLDB_PYTHON_RELATIVE_PATH=../Lib/site-packages ^
-    -DPYTHON_HOME=%PREFIX% ^
+    -DPython3_HOME=%PREFIX% ^
+    -DPython3_ROOT=%PREFIX% ^
     -DLLDB_EMBED_PYTHON_HOME=OFF ^
-    -DPYTHON_LIBRARIES=%PREFIX%/libs/python%PY_VER_NO_DOT%.lib ^
-    -DPYTHON_INCLUDE_DIRS=%PREFIX%/include ^
-    -DPYTHON_EXECUTABLE=%PREFIX%/python.exe ^
+    -DPython3_LIBRARIES=%PREFIX%/libs/python%PY_VER_NO_DOT%.lib ^
+    -DPython3_INCLUDE_DIRS=%PREFIX%/include ^
+    -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
     -DSWIG_EXECUTABLE=%LIBRARY_BIN%/swig.exe ^
     %SRC_DIR%\lldb
 if %ERRORLEVEL% neq 0 exit 1
